edff0f7f9bee915eae8836aaee93f21cc866e196
Tejasvi Vermani <112329218+tejasvi001@users.noreply.github.com>
2024-09-24 11:38:02 +0530

    Time: 17 ms (31.63%), Space: 54.9 MB (73.86%) - LeetHub

--- 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.java
+++ 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.java
@@ -1,12 +1,15 @@
 class Solution {
-    public List<Integer> findDuplicates(int[] arr) {
-       Arrays.sort(arr);
-       ArrayList<Integer> arrayList=new ArrayList<>();
-       for(int i=0;i<arr.length-1;i++){
-            if(arr[i]==arr[i+1]&&arrayList.contains(arr[i])==false){
-                arrayList.add(arr[i]);
+    public List<Integer> findDuplicates(int[] nums) {
+        ArrayList<Integer> obj=new ArrayList<>();
+        HashSet<Integer> list2=new HashSet<>();
+        for(int x:nums){
+            if(list2.contains(x)){
+                obj.add(x);
             }
-       }
-       return arrayList;
+            else{
+                list2.add(x);
+            }
+        }
+        return obj;
     }
 }
