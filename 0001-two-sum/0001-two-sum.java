1e78a4c0283a0df030f520f48cc76a21ddf7c161
Tejasvi Vermani <112329218+tejasvi001@users.noreply.github.com>
2024-10-08 10:30:02 +0530

    Time: 4 ms (63.4%), Space: 44.5 MB (93.77%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,8 +1,6 @@
 import java.util.Arrays;
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-      
-        
         int i=0;
         int j=nums.length-1;
         int arr1[]=new int[nums.length];
