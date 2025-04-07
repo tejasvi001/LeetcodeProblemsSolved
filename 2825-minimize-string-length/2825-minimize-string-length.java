fd3f613a14d6d74711b953b58083607f8e4124dd
Tejasvi Vermani <112329218+tejasvi001@users.noreply.github.com>
2025-04-07 08:34:51 +0530

    Time: 3 ms (99.55%), Space: 44.7 MB (93.83%) - LeetHub

--- 2825-minimize-string-length/2825-minimize-string-length.java
+++ 2825-minimize-string-length/2825-minimize-string-length.java
@@ -1,9 +1,18 @@
 class Solution {
     public int minimizedStringLength(String s) {
-        HashSet<Character> set=new HashSet<>();
-        for(int i=0;i<s.length();i++){
-            set.add(s.charAt(i));
-        }
-        return set.size();
+        // HashSet<Character> set=new HashSet<>();
+        // for(int i=0;i<s.length();i++){
+        //     set.add(s.charAt(i));
+        // }
+        // return set.size();
+       var map = new boolean[26];
+    var ans = 0;
+    
+    for (var c : s.toCharArray())
+      map[c - 'a'] = true;
+
+    for (var c : map) if (c) ans++;
+    
+    return ans;
     }
 }
