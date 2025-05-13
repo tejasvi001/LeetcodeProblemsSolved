65e7697af820dd4efd46c215ef3f038a8b1347c3
Tejasvi Vermani <112329218+tejasvi001@users.noreply.github.com>
2025-05-13 10:54:35 +0530

    Time: 6 ms (21.74%), Space: 47.1 MB (12.16%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.java
+++ 0338-counting-bits/0338-counting-bits.java
@@ -8,10 +8,10 @@ class Solution {
     }
     public int bits(int n){
         int res=0;
-        for (int i = 0; i < 32; i++) {
-            if (((n >> i) & 1) == 1) {
-                res ++;
-            }
+        while(n>0){
+            if((n&1)==1)
+                res++;
+            n=n>>1;
         }
         return res;
     }
