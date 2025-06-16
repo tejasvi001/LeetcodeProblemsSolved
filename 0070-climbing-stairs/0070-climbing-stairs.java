22093ac77f2850dcb017e549ca44cae112f54af6
Tejasvi Vermani <112329218+tejasvi001@users.noreply.github.com>
2025-06-16 12:24:34 +0530

    Time: 0 ms (100%), Space: 40.3 MB (65.8%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,17 +1,16 @@
 class Solution {
     public int climbStairs(int n) {
-        int arr[]=new int[n+1];
-        return climb(n,arr);
-    }
-    private int climb(int n,int arr[]){
         if(n<=2){
-            arr[n]=n;
-            return arr[n];
+            return n;
         }
-        if(arr[n]!=0){
-            return arr[n];
+        int dp[]=new int[n+1];
+        dp[0]=0;
+        dp[1]=1;
+        dp[2]=2;
+        for(int i=3;i<=n;i++){
+            dp[i]=dp[i-1]+dp[i-2];
         }
-        arr[n]=climb(n-1,arr)+climb(n-2,arr);
-        return arr[n];
+        return dp[n];
     }
+
 }
