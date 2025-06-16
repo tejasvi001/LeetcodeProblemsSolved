5485e7517a47bdebf1c05c7be570a92ed2be9aea
Tejasvi Vermani <112329218+tejasvi001@users.noreply.github.com>
2025-06-16 10:45:17 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,14 +1,8 @@
 class Solution {
     public int climbStairs(int n) {
-         if (n == 0 || n == 1) {
-            return 1;
+        if(n<=2){
+            return  n;
         }
-        int prev = 1, curr = 1;
-        for (int i = 2; i <= n; i++) {
-            int temp = curr;
-            curr = prev + curr;
-            prev = temp;
-        }
-        return curr;
+        return climbStairs(n-1)+climbStairs(n-2);
     }
 }
