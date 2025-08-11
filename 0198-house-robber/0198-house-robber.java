7b48f7069d01b9f6067645463bd1db99527d3ef7
Tejasvi Vermani <112329218+tejasvi001@users.noreply.github.com>
2025-08-11 14:47:39 +0530

    Time: 0 ms (100%), Space: 41.2 MB (45.95%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,16 +1,17 @@
 class Solution {
     public int rob(int[] nums) {
-           int n = nums.length;
-        int dp[] = new int[n+1];
-        Arrays.fill(dp,0);
-        dp[1] = nums[0];
-        for(int i = 1; i< n; i++){
-            int notTake = dp[i]; // 1 index shift or 1 based indexing
-            int take = nums[i] + dp[i-1];
-            dp[i+1] = Math.max(take,notTake);
-
+        if(nums.length==1){
+            return nums[0];
         }
-
-        return dp[n];
+        if(nums.length==0){
+            return -1;
+        }
+        int dp[]=new int[nums.length+1];
+        dp[0]=0;
+        dp[1]=nums[0];
+        for(int i=2;i<=nums.length;i++){
+            dp[i]=Math.max(dp[i-1],dp[i-2]+nums[i-1]);
+        }
+        return dp[nums.length];
     }
 }
