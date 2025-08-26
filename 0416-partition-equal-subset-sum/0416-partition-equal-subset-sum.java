267afebba08dcf1d6291d91ddd3645a040247fbc
Tejasvi Vermani <112329218+tejasvi001@users.noreply.github.com>
2025-08-26 11:21:29 +0530

    Time: 33 ms (70.18%), Space: 42.1 MB (89.74%) - LeetHub

---
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean canPartition(int[] nums) {
+        int totalSum = 0;
+        for (int num : nums) {
+            totalSum += num;
+        }
+        
+        // If sum is odd, cannot be partitioned
+        if (totalSum % 2 != 0) return false;
+        
+        int target = totalSum / 2;
+        int n = nums.length;
+        
+        // dp[i] = true if a subset with sum i can be formed
+        boolean[] dp = new boolean[target + 1];
+        dp[0] = true; // sum 0 is always possible (empty subset)
+        
+        for (int num : nums) {
+            // Traverse backwards to avoid reusing the same number
+            for (int j = target; j >= num; j--) {
+                dp[j] = dp[j] || dp[j - num];
+            }
+        }
+        
+        return dp[target];
+    }
+}
